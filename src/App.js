19629eae8f6dc27244d0e710a305e4ee199232ab
Dominic <48520590+Sevendaye@users.noreply.github.com>
2020-06-02 15:25:20 +0200

    finalisation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import marked from 'marked';
+import { sampleText } from './sampleText';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    text: sampleText
+  }
+
+  handleChange = (event) => {
+    let text = event.target.value;
+    this.setState({ text });
+  }
+
+  renderText = (text) => {
+    const __html = marked(text, { sanitize: true });
+    return { __html };
+  }
+
+  componentDidMount() {
+    const text = localStorage.getItem('text');
+
+    if (text) {
+      this.setState({ text })
+    } else {
+      this.setState({ text: sampleText })
+    }
+  }
+
+  componentDidUpdate() {
+    const text = this.state.text;
+    localStorage.setItem('text', text);
+  }
+
+  render() {
+    return (
+      <div className="container" >
+        <div className="row">
+          <div className="col-sm-6">
+            <textarea className="form-control" rows='35' value={this.state.text} onChange={this.handleChange}></textarea>
+          </div>
+          <div className="col-sm-6">
+            <div dangerouslySetInnerHTML={this.renderText(this.state.text)} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/sampleText.js
@@ -0,0 +1,1 @@
+export const sampleText = '# Titre\n## Sous-titres\n### Titre plus profond';
